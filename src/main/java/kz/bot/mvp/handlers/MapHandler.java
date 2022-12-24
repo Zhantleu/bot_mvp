007db55885828074e6c263708eeac3fbfcd7fa29
Айдос Жантлеу <a.zhantleu@kazmarka.kz>
2022-12-24 20:26:34 +0600

    Add seats.jpg

--- src/main/java/kz/bot/mvp/handlers/MapHandler.java
+++ src/main/java/kz/bot/mvp/handlers/MapHandler.java
@@ -2,6 +2,7 @@ package kz.bot.mvp.handlers;
 
 import kz.bot.mvp.models.SeatStatus;
 import kz.bot.mvp.storage.SeatStorage;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
@@ -12,21 +13,55 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
+import javax.imageio.ImageIO;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class MapHandler implements Handler {
 
+//    private final static
     private final SeatStorage seatStorage;
+
+    @SneakyThrows
+    public void init() {
+        InputFile SEATS =
+            new InputFile(TariffsHandler.class.getResourceAsStream("/seats.jpg"), "seats.jpg");
+        BufferedImage img = ImageIO.read(SEATS.getNewMediaStream());
+        Graphics2D g2d = img.createGraphics();
+        g2d.setColor(Color.RED);
+        g2d.drawRect(0, 0, 100, 100);
+        g2d.dispose();
+        try {
+            ImageIO.write(img, "png", SEATS.getNewMediaFile());
+        } catch (Exception e) {
+            System.out.println("[ERROR] Could not save image.");
+        }
+    }
 
     @Autowired
     public MapHandler(SeatStorage seatStorage) {
         this.seatStorage = seatStorage;
     }
 
+    @SneakyThrows
     @Override
     public PartialBotApiMethod<Message> process(String chatId) {
+        InputFile SEATS =
+            new InputFile(TariffsHandler.class.getResourceAsStream("/seats.jpg"), "seats.jpg");
+        BufferedImage img = ImageIO.read(SEATS.getNewMediaStream());
+        Graphics2D g2d = img.createGraphics();
+        g2d.setColor(Color.RED);
+        g2d.drawRect(0, 0, 100, 100);
+        g2d.dispose();
+        try {
+            ImageIO.write(img, "png", SEATS.getNewMediaFile());
+        } catch (Exception e) {
+            System.out.println("[ERROR] Could not save image.");
+        }
         return SendPhoto.builder()
             .chatId(chatId)
             .replyMarkup(
@@ -37,7 +72,7 @@ public class MapHandler implements Handler {
                         )
                     ).build()
             )
-            .photo(new InputFile(TariffsHandler.class.getResourceAsStream("/seats.jpg"), "seats.jpg"))
+            .photo(SEATS)
             .caption(getPretty())
             .build();
     }
