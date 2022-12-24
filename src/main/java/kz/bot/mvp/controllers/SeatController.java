ac2c2b3f888eebed565b9d875791bda7fc10c779
Айдос Жантлеу <a.zhantleu@kazmarka.kz>
2022-12-24 15:32:07 +0600

    fix controller

--- src/main/java/kz/bot/mvp/controllers/SeatController.java
+++ src/main/java/kz/bot/mvp/controllers/SeatController.java
@@ -1,5 +1,6 @@
 package kz.bot.mvp.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -7,7 +8,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/seats")
 public class SeatController {
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void save(@RequestBody Map<Integer, String> seatsToStatus) {
         System.out.println(seatsToStatus);
     }
