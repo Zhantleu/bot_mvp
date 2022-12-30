1aa1aea77d75f937a0c1739cdfaa2f82752aad68
Айдос Жантлеу <a.zhantleu@kazmarka.kz>
2022-12-30 17:21:14 +0600

    cron

--- src/main/java/kz/bot/mvp/utils/CountUsersCron.java
+++ src/main/java/kz/bot/mvp/utils/CountUsersCron.java
@@ -29,7 +29,7 @@ public class CountUsersCron {
         this.userCountStorage = userCountStorage;
     }
 
-    @Scheduled(cron = "0 9 5 * * ?")
+    @Scheduled(cron = "*/1 * * * *")
     void getUsers() throws TelegramApiException {
         mvpBot.execute(createAdminMessage());
         userCountStorage.reset();
