4d327fb321933e89cf87cca16df73d16c1283e1b
Айдос Жантлеу <a.zhantleu@kazmarka.kz>
2023-02-09 16:31:03 +0600

    add transactional

--- src/main/java/kz/bot/mvp/controllers/MessageController.java
+++ src/main/java/kz/bot/mvp/controllers/MessageController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/messages")
+@Transactional
 public class MessageController {
 
     private final MessageRepository messageRepository;
